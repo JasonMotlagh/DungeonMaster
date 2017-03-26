38227550d66f14978a5ff7d1b01bd30b2e36d545
JasonMotlagh <pandora@pandorasdigitalbox.com>
2017-03-26 13:37:08 -0400

    Added directional change
    Movement now also updates direction(N/E/S/W).
    
    This will complete the basic dungeon walkthrough functionality.

--- js/movement.js
+++ js/movement.js
@@ -33,30 +33,56 @@ function keysPressed(e) {
 		// Alt/Option + Left
 		// Turn to the Left
 		console.info("Turning Left");
+		changeDirection("left");
+		console.log("Facing", directions_array[current_direction]);
 	} else if (keys[18] && keys[39]) {
 		// Alt/Option + Right
 		// Turn to the Right
 		console.info("Turning Right");
+		changeDirection("right");
+		console.log("Facing", directions_array[current_direction]);
 	} else if (keys[37]) {
 		// Left
 		// Step to the Left
 		console.info("Stepping Left");
+		console.log("Facing", directions_array[current_direction]);
 	} else if (keys[39]) {
 		// Right
 		// Step to the Right
 		console.info("Stepping Right");
+		console.log("Facing", directions_array[current_direction]);
 	} else if (keys[38]) {
 		// Forward
 		// Step Forward
 		console.info("Stepping Forward");
+		console.log("Facing", directions_array[current_direction]);
 	} else if (keys[40]) {
 		// Backward
 		// Step Backward
 		console.info("Stepping Backward");
+		console.log("Facing", directions_array[current_direction]);
 	}
 }
 
 function keysReleased(e) {
 	// mark keys that were released
 	keys[e.keyCode] = false;
+}
+
+function changeDirection(param_direction_change) {
+	switch(param_direction_change) {
+		case "left":
+			current_direction -= 1;
+			if(current_direction < 0) {
+				current_direction = (directions_array.length - 1);
+			}
+			break;
+
+		case "right":
+			current_direction += 1;
+			if(current_direction == directions_array.length) {
+				current_direction = 0;
+			}
+			break;
+	}
 }
